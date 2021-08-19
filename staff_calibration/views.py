9463b82c34c1a6d19df83b3852af9d10fcf277c4
KhanduNakhap <79565221+KhanduNakhap@users.noreply.github.com>
2021-08-19 10:29:13 +0800

    Adding filters to Range Query

--- staff_calibration/views.py
+++ staff_calibration/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-import os, csv, io, numpy as np
+import csv, io, numpy as np
 from math import sqrt
 from .forms import StaffForm
 from .models import uCalibrationUpdate, uRawDataModel
@@ -9,10 +9,9 @@ from range_calibration.models import RangeParameters
 from datetime import date
 from django.contrib.auth.decorators import login_required 
 from django.core.exceptions import ObjectDoesNotExist
-from datetime import datetime, date
+from datetime import datetime
 from staffs.models import Staff
 from django.db.models import Q
-from django.conf import settings
 #from accounts.models import CustomUser
 # Create your views here.
 
@@ -53,9 +52,7 @@ def user_staff_delete(request, update_index):
 
 # handle data file
 def handle_uploaded_file(f):
-    root_dir = os.path.join(settings.UPLOAD_ROOT, 'client_data')
-    file_path = os.path.join(root_dir, f.name[:-4]+'-'+date.today().strftime('%Y%m%d')+'.csv')
-    # file_path = "data/client_data/"+f.name
+    file_path = "data/client_data/"+f.name
     with open(file_path, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
@@ -166,11 +163,11 @@ def calibrate(request):
             month = observation_date.strftime('%b')
             # Getting the range 
             range_value = RangeParameters.objects.values_list('pin', month)
-
-            if range_value.exists():
+            # if range_value.exists():
+            if range_value[0][1]:
                 # read file and data
                 # thisFile = request.FILES['document']
-                thisFile = handle_uploaded_file(data['document'])                                # Save file to path
+                thisFile = handle_uploaded_file(data['document'])                                # path to uploaded csv
                 # if thisFile.name.endswith('.csv') or thisFile.name.endswith('.txt'):
                 #     io_string = io.StringIO(thisFile.read().decode('utf-8'))
                 if thisFile.endswith('.csv') or thisFile.endswith('.txt'):
@@ -237,7 +234,7 @@ def calibrate(request):
                 return render(request, 'staff_calibration/staff_calibration_report.html', context)
                 #return redirect('staff_calibration:staff-guide')
             else:
-                messages.warning(request, 'No range measurements exist for the month of '+month+'. Please try again later.')
+                messages.warning(request, 'No range measurements exist for the month of '+month+'. Please try again later or contact Landgate')
                 return render(request, 'staff_calibration/staff_calibrate.html', {'form':form})
     else:
         form = StaffForm(user=request.user)
